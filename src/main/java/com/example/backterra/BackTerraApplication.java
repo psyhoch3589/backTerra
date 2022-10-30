ba7b78522c7e211b67a5410258e87b6f9f73025e
Anasa <aziach.anas@student.emi.ac.ma>
2022-10-30 15:18:55 +0100

    update

--- src/main/java/com/example/backterra/BackTerraApplication.java
+++ src/main/java/com/example/backterra/BackTerraApplication.java
@@ -8,6 +8,7 @@ public class BackTerraApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackTerraApplication.class, args);
+        System.out.println("ok");
     }
     //test
 }
