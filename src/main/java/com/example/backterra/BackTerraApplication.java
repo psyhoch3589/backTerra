ecadbbe72de26535fa7e2a5c1cfe4df8a7e845b1
Zakaria AIT <aitbenmesszakaria@gmail.com>
2022-10-30 14:45:41 +0100

    v1.0

---
+++ src/main/java/com/example/backterra/BackTerraApplication.java
@@ -0,0 +1,13 @@
+package com.example.backterra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTerraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackTerraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backterra/BackTerraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backterra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTerraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
