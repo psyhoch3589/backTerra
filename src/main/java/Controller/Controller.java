57736b2185a6a7214e533e38b4d4b933eb7446bf
Anasa <aziach.anas@student.emi.ac.ma>
2022-11-04 16:26:14 +0100

    V1.0 Architecture

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -6,7 +6,4 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    public User getUsers() {
-        return new User(1,"anas","anasaziach@gmail.com","anas");
-    }
 }
